d02f3ba196e724fdf1838ba6791c888adae87754
Valeri.Akseniko <valeri.akseniko@gmail.com>
2023-07-11 01:29:38 +0300

    added DI

---
+++ Hooks/DependencyInjectionHooks.cs
@@ -0,0 +1,31 @@
+﻿using BoDi;
+using FLS.AmazonPurchase.Pages;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace FLS.AmazonPurchase.Hooks
+{
+    [Binding]
+    public class DependencyInjectionHooks
+    {
+        private readonly IObjectContainer container;
+
+        public DependencyInjectionHooks(IObjectContainer container)
+        {
+            this.container = container;
+        }
+
+        [BeforeScenario]
+        public void DependencyRegister()
+        {
+            ChromeDriver driver = new ChromeDriver();
+            container.RegisterInstanceAs<IWebDriver>(driver);
+            container.RegisterInstanceAs<GooglePage>(new GooglePage(driver));
+            container.RegisterInstanceAs<AmazonPage>(new AmazonPage(driver));
+        }
+    }
+}
