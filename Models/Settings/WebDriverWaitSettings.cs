b46ff2f22c50c8f53a25a641d9cf204fc684f3f8
Valeri.Akseniko <valeri.akseniko@gmail.com>
2023-07-11 19:17:26 +0300

    update dev

---
+++ Models/Settings/WebDriverWaitSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FLS.AmazonPurchase.Models.Settings
+{
+    public class WebDriverWaitSettings
+    {
+        public int Timeout { get; set; }
+        public int PollingInterval { get; set; }
+    }
+}
